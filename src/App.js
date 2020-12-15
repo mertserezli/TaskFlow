d4ce7515b4c020fcdef176e815f5db8c3f0f5f32
Mert Serezli <mertserezli@gmail.com>
2020-12-15 11:50:35 +0300

    add layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
         <h1 style={{textAlign:"center"}}>Task Flow</h1>
-
+        <TaskFlow/>
     </div>
   );
+}
+
+function TaskFlow() {
+    return(
+    <div className="grid-container">
+        <div className="AddTask">
+
+        </div>
+        <div className="CurTask">
+
+        </div>
+        <div className="TaskList">
+
+        </div>
+    </div>
+    )
 }
 
 export default App;
